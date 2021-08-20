2959d868b47df26197bae58c32e9db268a3ca7c2
oolbru2s <66638753+oolbru2s@users.noreply.github.com>
2021-08-20 12:19:32 +0200

    init

---
+++ src/main/java/com/example/vocabextractor/VocabExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.example.vocabextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocabExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vocabextractor/VocabExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vocabextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
